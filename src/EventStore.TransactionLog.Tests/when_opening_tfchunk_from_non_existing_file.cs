2434afa6081b0565072f64c07e5796cf3548698a
chris.condron <chris@condron.net>
2020-07-13 18:38:06 -0400

    extract transaction log into dedicated projects

---
+++ src/EventStore.TransactionLog.Tests/when_opening_tfchunk_from_non_existing_file.cs
@@ -0,0 +1,15 @@
+using EventStore.Core.Exceptions;
+using EventStore.Core.TransactionLog.Chunks.TFChunk;
+using NUnit.Framework;
+
+namespace EventStore.Core.Tests.TransactionLog {
+	[TestFixture]
+	public class when_opening_tfchunk_from_non_existing_file : SpecificationWithFile {
+		[Test]
+		public void it_should_throw_a_file_not_found_exception() {
+			Assert.Throws<CorruptDatabaseException>(() => TFChunk.FromCompletedFile(Filename, verifyHash: true,
+				5, //Constants.TFChunkInitialReaderCountDefault,
+				21));//Constants.TFChunkMaxReaderCountDefault);
+		}
+	}
+}
